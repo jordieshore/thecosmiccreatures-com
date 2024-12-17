a83630912621c6ba663b6a4cfcd8f61a701191b8
jordieshore <jordanburgess789@gmail.com>
2024-12-17 05:42:22 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,441 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+
+
+    <div
+    className="relative bg-no-repeat w-full h-screen"
+    style={{
+      backgroundImage: `url('/61.png')`,
+      backgroundBlendMode: 'multiply',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center -380px',
+      backgroundRepeat: 'no-repeat',
+      height: '165vh', // Ensure this doesn't clip the Navbar
+    }}
+  >
+      <main>
+
+<nav className="flex justify-end space-x-8 p-4">
+
+<a href="https://discord.gg/QYhUNRPE9n" target="_blank" rel="nooper noreferrer">
+<span className="[&>svg]:h-8 [&>svg]:w-8">
+<svg
+xmlns="http://www.w3.org/2000/svg"
+fill="currentColor"
+viewBox="0 0 640 512">
+
+<path
+ d="M524.5 69.8a1.5 1.5 0 0 0 -.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 0 0 -1.9 .9 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 0 0 0 -15.1-30.6 1.9 1.9 0 0 0 -1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 0 -.8 .7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0 -1-2.6 321.2 321.2 0 0 1 -45.9-21.9 1.9 1.9 0 0 1 -.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9 .2c2.9 2.4 6 4.9 9.1 7.2a1.9 1.9 0 0 1 -.2 3.1 301.4 301.4 0 0 1 -45.9 21.8 1.9 1.9 0 0 0 -1 2.6 391.1 391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1 .7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 311 447.5 337.6 417.9 337.6z" />
+</svg>
+</span>
+</a>
+
+<a href="https://www.instagram.com/cosmiccreaturesxyz/" target="_blank" rel="nooper noreferrer">
+<span className="[&>svg]:h-8 [&>svg]:w-8">
+<svg
+xmlns="http://www.w3.org/2000/svg"
+fill="currentColor"
+viewBox="0 0 448 512">
+
+<path
+ d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+</svg>
+</span>
+</a>
+
+<a href="https://x.com/cosmicsxyz" target="_blank" rel="nooper noreferrer">
+<span className="[&>svg]:h-8 [&>svg]:w-8">
+<svg
+xmlns="http://www.w3.org/2000/svg"
+fill="currentColor"
+viewBox="0 0 512 512">
+
+<path
+ d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+</svg>
+</span>
+</a>
+
+</nav>
+
+
+        <div className="relative w-full h-full">
+  <div className="animate-float absolute top-[200px] left-[-220px] 
+                  sm:top-[-30px] sm:left-[-30px] 
+                  md:top-[220px] md:left-[-270px] 
+                  lg:top-[160px] lg:left-[-250px] 
+                  w-[150%] h-[100%] 
+                  sm:w-[90%] sm:h-[80%] 
+                  md:w-[80%] md:h-[85%] 
+                  lg:w-[60%] lg:h-[70%]">
+    <Image
+      src="/Untitled_Artwork 2.png"
+      alt="Overlay Image"
+      layout="intrinsic" 
+      width={2000} 
+      height={2000} 
+      objectFit="contain"
+      priority
+    />
+  </div>
+</div>
+
+
+<h1 className="orbitron-head  mt-[90px] sm:mt-[220px] md:mt-[207px] lg:mt-[250px] text-center md:text-center text-5xl md:text-[82px] lg:text-center leading-tight z-10 relative">
+  Cosmic <br />
+  Creatures
+</h1>
+
+
+
+      <div className="container mx-auto mt-[700px] md:mt-[750px] max-w-7xl py-8 lg:py-16">
+          
+        
+            <h2 className="kronaoneregular text-center text-[40px] z-10 relative">Guardians of <br/>
+            the Galactic Tapestry</h2>
+
+            <div className="hidden md:block">
+            <Image 
+              src="/ellipse17.png"
+              alt="Floating Image"
+              className="animate-float absolute ml-[50px] mt-[-50px] "
+              width={40} 
+              height={40} />
+
+            <Image 
+                src="/ellipse13.png"
+                alt="Floating Image"
+                className="animate-float1 absolute right-0 mr-[400px] mt-[80px]"
+                width={30} 
+                height={30} 
+                />
+
+            <Image 
+                src="/ellipse11.png"
+                alt="Floating Image"
+                className="animate-float2 absolute right-0 mr-[500px] mt-[-10px]"
+                width={20} 
+                height={20} 
+                />
+          </div>
+
+        
+       
+    
+
+        <div className="poppinsregular text-center mt-[25px] text-[20px] md:text-[20px] px-2 ">
+          <p className=" z-10 relative">Embark on an otherworldly journey with Cosmic Creatures, the original<br className="hidden md:block"/>
+          architects of the universe and guardians of galaxies.
+          </p> 
+          <p className="mt-[25px] px-2">
+          Each NFT in this creative project is a testament to ancient cosmic artistry,<br className="hidden md:block"/>
+          inviting you to become a custodian of the grand narrative.
+          </p>
+        </div>
+
+      </div>
+
+
+      <div className="flex flex-wrap justify-center gap-[60px] md:gap-[67px] mt-[50px] px-4">
+        <Image
+          src="/55.png"
+          alt="NFT"
+          className="w-[300px] h-[300px] object-cover rounded-[50px]"
+          width={500}
+          height={500}
+          
+        />
+        <Image
+          src="/79.png"
+          alt="NFT"
+          className="w-[300px] h-[300px] object-cover rounded-[50px]"
+           width={500}
+          height={500}
+        />
+        <Image
+          src="/78.png"
+          alt="Image 74"
+          className="w-[300px] h-[300px] object-cover rounded-[50px]"
+           width={500}
+          height={500}
+        />
+      </div>
+
+     
+      <button className="noto-sans-com text-[16px] mt-[50px] md:mt-[148px] notosans font-normal mx-auto text-center rounded-lg bg-[#4647C5] text-white px-5 py-4 hover:bg-[#3638A0] transition-colors duration-300 flex items-center justify-center z-10 relative">
+      Join Discord
+      </button>
+  
+
+      <div className="hidden md:block">
+        <Image 
+                  src="/ellipse12.png"
+                  alt="Floating Image"
+                  className="animate-float2 absolute ml-[330px] mt-[-30px] "
+                  width={50}
+          height={50}/>
+
+        <Image 
+                  src="/ellipse16.png"
+                  alt="Floating Image"
+                  className="animate-float absolute ml-[600px] mt-[60px] "
+                  width={20}
+                  height={20}/>
+      </div>
+
+  <div
+            className="bg-no-repeat w-full h-screen relative mt-[150px] md:mt-[258px]"
+            style={{
+              backgroundBlendMode: 'multiply',
+              backgroundImage: "url('/3.png')",
+              backgroundPosition: 'cover',
+              backgroundSize: 'cover',
+              backgroundRepeat: 'no-repeat',
+              position: 'relative',
+              height: '309vh'
+        
+            }}
+          >
+
+<div className="md:mb-[0px] mb-[350px]"></div>
+  <div className="container mx-auto px-4">
+  <div className="mx-auto mt-[150px] sm:mt-[170px] md:mt-[187px] lg:mt-[200px] max-w-7xl py-8 sm:py-10 md:py-12 lg:py-16">
+  <h2 className="kronaoneregular text-center mt-[-180px] sm:mt-[-150px] md:mt-[-70px] lg:mt-[-50px] text-[28px] sm:text-[30px] md:text-[40px] lg:text-[45px]">
+    Limited Edition Stardust, Infinite Journeys
+  </h2>
+  <p className="poppinsregular text-center mt-[30px] sm:mt-[40px] md:mt-[60px] lg:mt-[70px] text-[18px] sm:text-[20px] md:text-[22px] px-4 sm:px-6 md:px-8 lg:px-10">
+    Secure your link to the cosmic grid, own a piece of creative genesis, and join<br className="hidden lg:block"/>
+    a community of cosmic explorers. Explore the cosmos, ignite your creativity,<br className="hidden lg:block"/>
+    and embrace the timeless allure of the Cosmic Creatures NFT project.
+  </p>
+  <p className="poppinsregular text-center mt-4 sm:mt-5 md:mt-6 lg:mt-7 text-[18px] sm:text-[20px] md:text-[22px]">
+  <strong>
+    Seize the opportunity to capture a fragment of eternity.<br className="hidden md:block"/> 
+    The Cosmic Creatures Collection awaits, where the dance of stars<br className="hidden md:block"/>
+ 
+    becomes a timeless journey.
+    </strong>
+  </p>
+</div>
+
+<div className="hidden md:block" >
+        <Image 
+                    src="/ellipse10.png"
+                    alt="Floating Image"
+                    className="animate-float2 absolute right-0 mr-[150px] mt-[150px]"
+                    width={30} 
+                    height={30} />
+
+</div>
+
+<div className="relative">
+  <Image
+    src="/head_Artwork 6.png"
+    alt="Overlay Image"
+    width={800}
+    height={800}
+    className="absolute lg:hidden 
+              ml-[16px]
+              mt-[-29px] 
+              sm:top-[50px] 
+              sm:left-[43px] 
+              sm:w-[800px] 
+              sm:mt-[1350px] 
+
+              md:top-[-1350px] 
+              md:left-[324px] 
+              md:w-[500px] 
+
+              lg:top-[50px] 
+              lg:left-[43px] 
+              lg:w-[800px] 
+              lg:mt-[800px] 
+              object-contain overflow-visible"
+    priority
+  />
+</div>
+
+<div className="pl-4 sm:pl-[50px] md:pl-[170px] mt-[620px] sm:mt-[30px] md:mt-[670px] py-8 sm:py-8 md:py-12 lg:py-16 block relative z-10">
+  <h2 className="kronaoneregular text-left text-[24px] sm:text-[32px] md:text-[40px]">
+    Embodied Stardust:<br /> 
+    A Timeless Elegance
+  </h2>
+
+  <p className="poppinsregular mt-5 sm:mt-[30px] md:mt-[46px] text-[16px] sm:text-[18px] md:text-[20px]">
+  <strong>Stellar Origins:</strong> Fashioned from the raw materials of the
+  <br className="hidden lg:block" />
+  cosmos, each Cosmic Creature is a testament to the beauty
+  <br className="hidden lg:block" />
+  of celestial birth.
+</p>
+
+<p className="poppinsregular mt-4 sm:mt-5 md:mt-5 text-[16px] sm:text-[18px] md:text-[20px]">
+  <strong>Timeless Drift:</strong> As entities beyond time, these ethereal beings
+  <br className="hidden lg:block" />
+  gracefully navigate the cosmic currents, their existence
+  <br className="hidden lg:block" />
+  an ever-flowing melody.
+</p>
+
+<p className="poppinsregular mt-4 sm:mt-5 md:mt-5 text-[16px] sm:text-[18px] md:text-[20px]">
+  <strong>Etheric Stories:</strong> Own a piece of the universe&apos;s narrative, as
+  <br className="hidden lg:block" />
+  each Creature carries with it a tale woven into the fabric of
+  <br className="hidden lg:block" />
+  galaxies.
+</p>
+
+
+  <div className="relative w-full"></div>
+</div>
+
+<div className="relative hidden lg:block">
+  <Image 
+    src="/Untitled_Artwork.png"
+    alt="Overlay Image"
+    width={800} 
+    height={800} 
+    className="absolute top-[-1310px] left-[72.8%] transform -translate-x-1/2 w-[80vw] lg:w-[55vw] h-auto object-contain"
+    priority 
+  />
+</div>
+
+
+
+
+<div className="mx-auto text-center sm:mt-[250px] mt-[150px] md:mt-[400px] lg:mt-[730px] max-w-7xl py-12 sm:py-8 lg:py-16">
+  <h2 className="kronaoneregular text-center text-[40px] z-10">Join the Community</h2>
+  
+  <div className="hidden md:block">
+    <Image 
+      src="/ellipse11.png"
+      alt="Floating Image"
+      className="animate-float absolute right-0 mr-[760px] mt-[35px]"
+      width={28} 
+      height={28} 
+    />
+  </div>
+
+  <p className="poppinsregular text-center mt-[48px] text-[20px]">
+    Celestial Exploration: Immerse yourself in the Cosmic Creatures Collection, where<br className="hidden md:block"/> the stories of the stars come to life.
+  </p>
+
+  <p className="poppinsregular text-center mt-5 text-[20px] z-10 relative">
+    Connect with the Cosmos: Link your wallet to the cosmic grid and embrace<br/> the ethereal connection to the original essence of time.
+  </p>
+
+  <div className="hidden md:block">
+    <Image 
+      src="/ellipse13.png"
+      alt="Floating Image"
+      className="animate-float1 absolute right-0 mr-[475px] mt-[-4px]"
+      width={25} 
+      height={25} 
+    />
+    <Image 
+      src="/ellipse12.png"
+      alt="Floating Image"
+      className="animate-float absolute ml-[100px] mt-[25px]"
+      width={40} 
+      height={40} 
+    />
+  </div>
+
+  <p className="poppinsregular text-center mt-5 text-[20px] z-10 relative">
+    Harvest Stardust: Choose your Cosmic Creature and become a custodian<br /> of the celestial heritage, a keeper of stories untold.
+  </p>
+
+  <button className=" noto-sans-com text-[16px] mt-[40px] notosans font-normal mx-auto text-center rounded-lg bg-[#7879F1] text-white px-5 py-4 hover:bg-[#3638A0] transition-colors duration-300 flex items-center justify-center">
+    Join our community
+  </button>
+</div>
+
+
+
+
+    </div>
+
+ 
+</div>
+
+
+        
+      
+<div className="relative mb-[30px] sm:mb-[60px] mt-[-880px] sm:mt-[600px] md:mt-[331px]">
+  <div className="hidden md:block">
+    <Image 
+      src="/ellipse14.png"
+      alt="Floating Image"
+      className="animate-float1 absolute ml-[370px] mt-[-40px]"
+      width={18} 
+      height={18} 
+    />
+  </div>
+ <h3 className="kronaoneregular  mt-[450px] sm:mt-[-1500px] md:mt-[-200px] lg:mt-[-10px] mx-auto text-center text-[20px] sm:text-[22px] md:text-[24px] lg:text-[26px] justify-center px-4 sm:px-6 md:px-8 lg:px-10">
+  From stardust to eternity.<br className="block sm:hidden" /> Explore the cosmos. <br />
+  <p className="mt-[10px] sm:mt-[5px] md:mt-[0px] lg:mt-[-10px]">
+    Join the timeless journey of the Cosmic Creatures Collection.
+  </p>
+</h3>
+
+</div>
+
+
+
+      </main>
+
+      
+      <footer className="relative z-50 flex justify-center items-center space-x-6 py-6 sm:mt-[100px] md:mt-[30px] lg:mt-[0px]">
+      {/* Discord 1 */}
+      <a href="https://discord.gg/QYhUNRPE9n" target="_blank" rel="nooper noreferrer">
+      <div className="flex justify-center items-center h-12 w-12 bg-[#4647C5]/70 border-2 border-[#4647C5]/70 rounded-full">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 640 512"
+          className="h-6 w-6 text-white"
+          fill="currentColor"
+        >
+          <path d="M524.5 69.8a1.5 1.5 0 0 0-.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 0 0-1.9.9 337.5 337.5 0 0 0-14.9 30.6 447.8 447.8 0 0 0-134.4 0 309.5 309.5 0 0 0-15.1-30.6 1.9 1.9 0 0 0-1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 0-.8.7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0-1-2.6 321.2 321.2 0 0 1-45.9-21.9 1.9 1.9 0 0 1-.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9.2c2.9 2.4 6 4.9 9.1 7.2a1.9 1.9 0 0 1-.2 3.1 301.4 301.4 0 0 1-45.9 21.8 1.9 1.9 0 0 0-1 2.6 391.1 391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1.7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 311 447.5 337.6 417.9 337.6z" />
+        </svg>
+      </div>
+      </a>
+
+      {/* Instagram 2 */}
+      <a href="https://www.instagram.com/cosmiccreaturesxyz/" target="_blank" rel="nooper noreferrer">
+
+      <div className="flex justify-center items-center h-12 w-12 bg-[#4647C5]/70 border-2 border-[#4647C5]/70 rounded-full">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+          className="h-6 w-6 text-white"
+          fill="currentColor"
+        >
+          <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+        </svg>
+      </div>
+      </a>
+
+      {/* twitter 3 */}
+      <a href="https://x.com/cosmicsxyz" target="_blank" rel="noopener noreferrer">
+
+      <div className="flex justify-center items-center h-12 w-12 bg-[#4647C5]/70 border-2 border-[#4647C5]/70 rounded-full">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 576 512"
+          className="h-6 w-6 text-white"
+          fill="currentColor"
+        >
+          <path
+          d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+          </svg>
+      </div>
+      </a>
+    </footer>
+
+    </div>
+  
+  );
+}
